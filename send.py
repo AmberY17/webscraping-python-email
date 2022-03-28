700ee6143692e0c4596a54b06f986bc13745ca7a
Amber Yang <a227yang@uwaterloo.ca>
2022-03-28 19:25:42 -0400

    Set up automated/scheduled email

--- send.py
+++ send.py
@@ -1,4 +1,6 @@
 import smtplib 
+import schedule
+import time
 from account import *
 from email.message import EmailMessage
 import datetime
@@ -11,13 +13,21 @@ msg["From"] = EMAIL_ADDRESS
 msg["To"] = "ysy011600@gmail.com"
 msg.set_content(scrape_weather() + "\n" + scrape_news() + "\n" + scrape_quotes())
 
+def mail():
+  with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    # Connection Check
+    smtp.ehlo() 
+    # Encoding
+    smtp.starttls()
+    # Login
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    
+    smtp.send_message(msg)
 
-with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
-  # Connection Check
-  smtp.ehlo() 
-  # Encoding
-  smtp.starttls()
-  # Login
-  smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-  
-  smtp.send_message(msg)
+    smtp.quit()
+
+schedule.every().day.at("9:00").do(mail)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
